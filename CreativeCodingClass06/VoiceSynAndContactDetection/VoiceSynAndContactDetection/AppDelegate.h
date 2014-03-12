6c04ce91d6731a22873a7c4d0d98bab98aeacead
Quincy Bock <qkbock@gmail.com>
2014-03-12 17:23:51 -0400

    week 6

---
+++ CreativeCodingClass06/VoiceSynAndContactDetection/VoiceSynAndContactDetection/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VoiceSynAndContactDetection
+//
+//  Created by Quincy Bock on 3/4/14.
+//  Copyright (c) 2014 Quincy Bock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
